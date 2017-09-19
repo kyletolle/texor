e7aecbaf50e7d77d1549cc1cc641e947a83b3ac0
Kyle Tolle <kyle@nullsix.com>
2017-09-19 13:42:23 -0700

    Clear screen on exit and die

--- texor.c
+++ texor.c
@@ -17,6 +17,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
   perror(s);
   exit(1);
 }
@@ -64,6 +66,8 @@ void editorProcessKeypress() {
 
   switch(c) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
