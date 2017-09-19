1bc605fd7e0ec6756da47a52dc79698003b73d6e
Kyle Tolle <kyle@nullsix.com>
2017-09-19 13:30:08 -0700

    Make ctrl-q quit

--- texor.c
+++ texor.c
@@ -6,6 +6,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -50,7 +54,7 @@ int main() {
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   }
   return 0;
 }
