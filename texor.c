c805dbe3959482da65471715dfed5e9eb9984d7a
Kyle Tolle <kyle@nullsix.com>
2017-10-16 22:34:53 -0400

    Add a welcome message

--- texor.c
+++ texor.c
@@ -10,6 +10,8 @@
 
 /*** defines ***/
 
+#define TEXOR_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
@@ -120,7 +122,22 @@ void abFree(struct abuf *ab) {
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Texor editor -- version %s", TEXOR_VERSION);
+      if (welcomelen > E.screencols) welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 2);
+        padding--;
+      }
+      while(padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
