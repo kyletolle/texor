ea86f9874589b1b26953de8cc83009afbb62b778
Kyle Tolle <kyle@nullsix.com>
2017-10-23 14:22:04 -0400

    Snap cursor to end of line

--- texor.c
+++ texor.c
@@ -318,6 +318,12 @@ void editorMoveCursor(int key) {
       }
       break;
   }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
 }
 
 void editorProcessKeypress() {
