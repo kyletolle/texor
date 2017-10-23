470674d0211f2dba93e574d4affedf5433302cf1
Kyle Tolle <kyle@nullsix.com>
2017-10-23 14:18:30 -0400

    Restrict moving cursor past end of lines

--- texor.c
+++ texor.c
@@ -294,6 +294,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -301,7 +303,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (row && E.cx < row->size) {
+        E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
