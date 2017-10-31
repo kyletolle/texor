f3776c0cdcb4b564303a74d04345be7e1ff6bee1
Kyle Tolle <kyle@nullsix.com>
2017-10-31 13:35:19 -0600

    Allow canceling save as dialog with ESC

--- texor.c
+++ texor.c
@@ -362,7 +362,11 @@ void editorOpen(char *filename) {
 
 void editorSave() {
   if (E.filename == NULL) {
-    E.filename = editorPrompt("Save as: %s");
+    E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+    if (E.filename == NULL) {
+      editorSetStatusMessage("Save aborted");
+      return;
+    }
   }
 
   int len;
@@ -540,7 +544,11 @@ char *editorPrompt(char *prompt) {
     editorRefreshScreen();
 
     int c = editorReadKey();
-    if (c == '\r') {
+    if (c == '\x1b') {
+      editorSetStatusMessage("");
+      free(buf);
+      return NULL;
+    } else if (c == '\r') {
       if (buflen != 0) {
         editorSetStatusMessage("");
         return buf;
